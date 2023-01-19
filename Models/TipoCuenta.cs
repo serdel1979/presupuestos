0d54e3fac4361e8b2b2095ccb29e4969af104fc8
Sergio De Luca <sdlbsso@gmail.com>
2023-01-19 00:00:57 -0300

    Controlador de tipo de cuenta y vista crear

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Presupuestos.Models
 {
@@ -6,6 +7,7 @@ namespace Presupuestos.Models
     {
         public int id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido!!!")]
+        [StringLength(maximumLength:50,MinimumLength =5, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
         public string nombre { get; set; }
         public int usuarioid { get; set;}
         public int orden { get; set; }
