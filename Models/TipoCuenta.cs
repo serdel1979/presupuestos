37f7e7266c29ab69ed725a716f35d878810b7299
Sergio De Luca <sdlbsso@gmail.com>
2023-01-24 19:45:20 -0300

    Validaciones en crear cuenta

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -5,11 +5,19 @@ namespace Presupuestos.Models
 {
     public class TipoCuenta
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido!!!")]
         [StringLength(maximumLength:50,MinimumLength =5, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
-        public string nombre { get; set; }
-        public int usuarioid { get; set;}
-        public int orden { get; set; }
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido!!!")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un email válido!!!")]
+        public string Email { get; set; }
+        [Url(ErrorMessage = "El campo {0} debe ser una url válida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage = "El campo {0} debe ser una número tarjeta de crédito válida")]
+        public string TarjetaDeCredito { get; set; }
+
     }
 }
